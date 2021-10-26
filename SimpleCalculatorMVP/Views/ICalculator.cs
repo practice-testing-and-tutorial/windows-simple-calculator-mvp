6968806c1cdee30f7e57713b177a08cebfaada0f
Wahyu <wahjoe.eko.n@gmail.com>
2021-10-26 10:01:52 +0700

    [add] ICalculator to Views

---
+++ SimpleCalculatorMVP/Views/ICalculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculatorMVP.Views
+{
+	interface ICalculator
+	{
+		string TxtNumberOne { get; set; }
+		string TxtNumberTwo { get; set; }
+		string TxtResult { get; set; }
+	}
+}
