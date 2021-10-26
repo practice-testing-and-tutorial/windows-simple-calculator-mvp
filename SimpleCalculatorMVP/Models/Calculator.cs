a56d2628b26444dbfeb4e0a9156c995d230ffb87
Wahyu <wahjoe.eko.n@gmail.com>
2021-10-26 09:58:28 +0700

    [add] Calculator to Models

---
+++ SimpleCalculatorMVP/Models/Calculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculatorMVP.Models
+{
+	public class Calculator
+	{
+		public double NumberOne { get; set; }
+		public double NumberTwo { get; set; }
+
+		public double CalculateSumation() => NumberOne + NumberTwo;
+		public double CalculateSubtraction() => NumberOne - NumberTwo;
+		public double CalculateMultiplication() => NumberOne * NumberTwo;
+		public double CalculateDivition() => NumberOne / NumberTwo;
+	}
+}
